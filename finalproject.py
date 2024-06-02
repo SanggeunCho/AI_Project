de248cf2cefee9d2367cb082206f1f91ce04673d
SanggeunCho <143462558+SanggeunCho@users.noreply.github.com>
2024-06-03 03:08:24 +0900

    MAE changed

--- finalproject.py
+++ finalproject.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
 
 # Get data from the file
@@ -106,9 +106,9 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 # Evalute the model
-mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
-print(f'Mean Squared Error: {mse}')
+print(f'Mean Absolute Error: {mae}')
 print(f'R^2 Score: {r2}')
 
 # Plot of predicted values and actual values
